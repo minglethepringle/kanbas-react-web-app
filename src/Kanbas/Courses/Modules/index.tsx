e9e822addcc85ec65da584390a98b691d14aa1de
Mingle Li <limingle5@gmail.com>
2024-10-07 13:30:09 -0400

    4.7

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -41,12 +41,25 @@ export default function Modules() {
                     </ul>
                 </li>
                 <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-                    <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+                    <div className="wd-title p-3 ps-2 bg-secondary">
+                        <BsGripVertical className="me-2 fs-3" /> Week 2
+                        <ModuleControlButtons /></div>
                     <ul className="wd-lessons list-group rounded-0">
                         <li className="wd-lesson list-group-item p-3 ps-1">
-                            LEARNING OBJECTIVES </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
+                            <BsGripVertical className="me-2 fs-3" />
+                            LEARNING OBJECTIVES
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1">
+                            <BsGripVertical className="me-2 fs-3" />
+                            LESSON 1
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1">
+                            <BsGripVertical className="me-2 fs-3" />
+                            LESSON 2
+                            <LessonControlButtons />
+                        </li>
                     </ul>
                 </li>
             </ul>
